eaa80297feeee32650fee705a8acfbd112aad8a2
James Page <james.page6978@gmail.com>
2018-06-06 13:01:06 +0000

    added dict functionalty can be expanded to as many sites and data collection as possible

--- web_scraper_weather_data_collect.py
+++ web_scraper_weather_data_collect.py
@@ -5,46 +5,51 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import sys
-page = requests.get("https://www.metoffice.gov.uk/public/weather/forecast/gchzjb39w#?date=2018-06-06")
 
-soup = BeautifulSoup(page.content, 'html.parser')
+URLS = {"Carmarthen":"https://www.metoffice.gov.uk/public/weather/forecast/gchzjb39w#?date=2018-06-06"}
 
-sevenday = soup.find(id="dayNav")
-today = sevenday.find(id="tabDay0")
-date = today["data-date"]
-day_temp = (today.select(".dayTemp"))[0]["data-value-raw"]
-night_temp = (today.select(".nightTemp"))[0]["data-value-raw"]
-uv_pol = today.select("i")
+for key,val in URLS.items():
 
-if uv_pol[0]["data-type"] == "pollen":
-	pollen = uv_pol[0].get_text()
-else:
-	pollen = "NA"
-	
+	page = requests.get(val)
 
-if uv_pol[1]["data-type"] == "uv":
-	uv = uv_pol[1]["data-value"]
-else:
-	uv = "NA"
+	soup = BeautifulSoup(page.content, 'html.parser')
 
-short_descs = [d["alt"] for d in today.select("img")]
+	sevenday = soup.find(id="dayNav")
+	today = sevenday.find(id="tabDay0")
+	date = today["data-date"]
+	day_temp = (today.select(".dayTemp"))[0]["data-value-raw"]
+	night_temp = (today.select(".nightTemp"))[0]["data-value-raw"]
+	uv_pol = today.select("i")
 
+	if uv_pol[0]["data-type"] == "pollen":
+		pollen = uv_pol[0].get_text()
+	else:
+		pollen = "NA"
+		
 
+	if uv_pol[1]["data-type"] == "uv":
+		uv = uv_pol[1]["data-value"]
+	else:
+		uv = "NA"
 
-weather = pd.DataFrame({
-		"Day" : date,
-		"short_desc": short_descs,
-		"Day_time_temps": day_temp,
-		"Night_time_temps": night_temp,
-		"Pollen": pollen,
-		"UV": uv
-	})
+	short_descs = [d["alt"] for d in today.select("img")]
 
 
-print("#############################################################")
-print(weather)
-print("#############################################################")
-# weather.to_csv("weather.csv",mode='a', index=False)
+
+	weather = pd.DataFrame({
+			"Day" : date,
+			"short_desc": short_descs,
+			"Day_time_temps": day_temp,
+			"Night_time_temps": night_temp,
+			"Pollen": pollen,
+			"UV": uv
+		})
+
+
+	print("#############################################################")
+	print(weather)
+	print("#############################################################")
+	weather.to_csv(key + "_weather.csv",mode='a', index=False)
 
 
 # #plt.plot(periods,day_temps,'*')
